8a68f9775099b1e684b806738bf6a3a54f7a9a85
desmondyst <desmondyong06@gmail.com>
2022-12-28 14:33:47 +0800

    Styling changes

--- base/urls.py
+++ base/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path("profile/<str:pk>/", views.userProfile, name="user-profile"),
 
 
-    path('create-room/', views.createRoom, name="create-room"),
+    path('create-post/', views.createRoom, name="create-post"),
     path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
     path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
     path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),

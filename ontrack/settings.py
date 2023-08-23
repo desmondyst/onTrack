4c5e0ac9f8fe7c5028c86a5a415ab4eeeca68943
desmondyst <desmondyong06@gmail.com>
2023-08-23 23:52:11 +0800

    fix favicon

--- ontrack/settings.py
+++ ontrack/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-hojawnnw4=9ua*s9@uu)x)7f$i_65a&zg9r14s@mw@$b8=ht8(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

c78fa2e6fb90f4d596d0b1cdc4e538f1f3aa5eff
desmondyst <desmondyong06@gmail.com>
2023-10-23 20:55:24 +0800

    :construction: trying out postgreSQL

--- base/models.py
+++ base/models.py
@@ -1,21 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     # null =True because alrdy has user here, dw conflict in database?
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
 
-    avatar = models.ImageField(null = True, default="avatar.svg")
+    avatar = models.ImageField(null=True, default="avatar.svg")
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
     # REQUIRED_FIELDS must contain all required fields on your user model, but should not contain the USERNAME_FIELD or password as these fields will always be prompted for.
-    REQUIRED_FIELDS = []
-
+    REQUIRED_FIELDS = ["username"]
 
 
 # Create your models here.
+
 
 class Topic(models.Model):
     name = models.CharField(max_length=200)
@@ -25,23 +26,22 @@ class Topic(models.Model):
 
 
 class Room(models.Model):
-   
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
-    #blank = True is for the saving of the form when the form is blank
+    # blank = True is for the saving of the form when the form is blank
     description = models.TextField(null=True, blank=True)
     # The related_name attribute specifies the name of the reverse relation from the User model back to your model.
-    participants = models.ManyToManyField(User, related_name='participants', blank=True)
+    participants = models.ManyToManyField(User, related_name="participants", blank=True)
 
-    #everytime the save method is called, go ahead and take the timestamp
+    # everytime the save method is called, go ahead and take the timestamp
     updated = models.DateTimeField(auto_now=True)
-    #take timestamp when first create
+    # take timestamp when first create
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         # specify ordering, most recently updated first then created
-        ordering = ['-updated', '-created']
+        ordering = ["-updated", "-created"]
 
     def __str__(self):
         return self.name
@@ -57,7 +57,7 @@ class Message(models.Model):
 
     class Meta:
         # specify ordering, most recently updated first then created
-        ordering = ['-updated', '-created']
+        ordering = ["-updated", "-created"]
 
     def __str__(self):
         return self.body[0:50]
